8afec11f42f4f232bb2d3f875d644798038ff68d
Jorge Matias <jmatias@assia-inc.com>
2021-08-04 15:58:19 +0200

    Fixed hello 3

--- module2/src/main/java/io/jitpack/App.java
+++ module2/src/main/java/io/jitpack/App.java
@@ -6,7 +6,7 @@ package io.jitpack;
  */
 public class App 
 {
-    public static String GREETING = "Hello World!";
+    public static String GREETING = "Hello World 3!";
     
     public static void main( String[] args )
     {
